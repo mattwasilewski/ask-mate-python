ed9889e444ba73201ba11244afda163d5b519a95
lolekgk <karol.gajda97@gmail.com>
2022-04-19 17:12:28 +0200

    Add /list route

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import data_manager
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/list")
+def route_list():
+    date = data_manager.timestamp_to_datetime()
+    answers, questions = data_manager.get_all_data()
+    return render_template('list.html', questions=questions, date=date)
 
 
 if __name__ == "__main__":

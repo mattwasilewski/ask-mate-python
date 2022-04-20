d1c605dce5f39dd4f1ddfedc0c1f97f7f8b8d552
lolekgk <karol.gajda97@gmail.com>
2022-04-20 15:21:13 +0200

    Test new features

--- server.py
+++ server.py
@@ -9,18 +9,29 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/list")
+@app.route("/list", methods=['GET', 'POST'])
 def route_list():
     date = data_manager.timestamp_to_datetime()
     answers, questions = data_manager.get_all_data()
+    questions = sorted(questions, key=lambda item: item['submission_time'])
     # if wybrany z options rodzaj sortowania
     # return list html z wybranym rodzajem sortowania w jako klucz słownika
     # np. sorted(questions, key=lambda item: item['view_number'], reverse=True)
     return render_template('list.html',
-                           questions=sorted(questions, key=lambda item: item['submission_time']),
+                           questions=questions,
                            date=date)
 
 
+@app.route("/list?order_by=<sort>", methods=['GET', 'POST'])
+def filters(sort=None):
+    answers, questions = data_manager.get_all_data()
+    date = data_manager.timestamp_to_datetime()
+    sort = request.form.get('sort')
+    if sort == 'title':
+        questions = sorted(questions, key=lambda item: item['title'])
+        return render_template('list.html', questions=questions, date=date)
+
+# /list?order_by=title&order_direction=desc
 
 if __name__ == "__main__":
     app.run(

1cd0e4a3debb5ca17911a0ccc8278d58f905e1a6
lolekgk <karol.gajda97@gmail.com>
2022-05-18 13:39:27 +0200

    Add table in user-page

--- server.py
+++ server.py
@@ -265,16 +265,13 @@ def logout():
 def user_list():
     username = session.get('username')
     user_id = data_manager.get_user_data_by_username(username).get('id') if username else None
-    usersdata = data_manager.get_users_data()
+    users_data = data_manager.get_users_data()
     count_questions = data_manager.count_data_by_user_id('question')
-    print(count_questions)
     count_answers = data_manager.count_data_by_user_id('answer')
     count_comments = data_manager.count_data_by_user_id('comment')
-    registration_date = data_manager.get_users_data()
-    return render_template('user-list.html', usersdata=usersdata, count_questions=count_questions,
+    return render_template('user-list.html', usersdata=users_data, count_questions=count_questions,
                            count_answers=count_answers, count_comments=count_comments,
-                           registration_date=registration_date, username=username,
-                           user_id=user_id)
+                           username=username, user_id=user_id)
 
 
 @app.route('/user/<int:user_id>')
@@ -282,8 +279,16 @@ def user_page(user_id):
     if not session.get('username'):
         flash("You can not access this page.", 'warning')
         return redirect(url_for('main_page'))
-    userdata = data_manager.get_user_data_by_username(session['username'])
-    return render_template('user-page.html', username=userdata['username'], user_id=user_id)
+    username = session.get('username')
+    user_id = data_manager.get_user_data_by_username(username).get('id') if username else None
+    userdata = data_manager.get_user_data_by_username(username)
+    print(userdata)
+    count_questions = data_manager.count_data_by_user_id('question')
+    count_answers = data_manager.count_data_by_user_id('answer')
+    count_comments = data_manager.count_data_by_user_id('comment')
+    return render_template('user-page.html', userdata=userdata, count_questions=count_questions,
+                           count_answers=count_answers, count_comments=count_comments,
+                           username=username, user_id=user_id)
 
 
 if __name__ == "__main__":
